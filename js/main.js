7155a1ea7571dfaac4aba580dbf75683b48b16c4
Viknesh <valagapp@umich.edu>
2025-12-26 15:08:51 -0500

    Fix bug

--- js/main.js
+++ js/main.js
@@ -40,7 +40,7 @@ $(function () {
         });
         answerOpen=document.getElementById("answerOpen").checked;
         setupScore();
-        if(answerOpen) $('.passBtn).hide();
+        if(answerOpen) $('.passBtn').hide();
         penalty = $('#wrongPenalty').val();
         disableScore();
         $('#inputContainer').hide();
